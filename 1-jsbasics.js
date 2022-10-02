d332ea95f168cea16617bd823ce1432955395e0d
Eugene Lobach <ucta456@gmail.com>
2022-10-02 11:31:53 +0300

    1Lesson

---
+++ 1-jsbasics.js
@@ -0,0 +1,8 @@
+let user = prompt("Введите имя");
+console.log(`Привет, ${user}!`);
+alert(`Привет, ${user}!`);
+
+let number = prompt("Введите число");
+let degree = prompt("Введите степень");
+
+alert(`Число ${number} в степени ${degree} = ${Math.pow(number, degree)}`);
