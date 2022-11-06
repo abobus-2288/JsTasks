386ba6f14a22dbdaed4800d83b494318dc6aecd3
Eugene <ucta456@gmail.com>
2022-11-06 12:19:01 +0300

    2Lesson Remade

--- 2.js
+++ 2.js
@@ -1,68 +1,68 @@
 // // N-1
-// let var1 = "hidden";
-// var1 == "hidden" ? var1 = "visible" : var1 = "hidden";
+// let isHidden = "hidden";
+// isHidden === "hidden" ? isHidden = "visible" : isHidden = "hidden";
 
 // // N-2
 //
-// let var2 = 0;
+// let number2 = 0;
 //
-// if (var2 == 0) {
-//     var2 = 1;
+// if (number2 == 0) {
+//     number2 = 1;
 // }
 //
-// else if (var2 < 0) {
-//     var2 = "less then zero";
+// else if (number2 < 0) {
+//     number2 = "less then zero";
 // }
 //
-// else if (var2 > 0) {
-//     var2 *= 10;
+// else if (number2 > 0) {
+//     number2 *= 10;
 // }
 
 // // N-3
 //
-// let var3 = prompt("Введите число (Задание 3)");
+// let prompt3 = prompt("Введите число (Задание 3)");
 //
-// let var3Total = Number(var3) < 5 ? 0 : 1;
+// let number3Total = Number(number3) < 5 ? 0 : 1;
 
 // // N-4
 //
-// let var3_1 = prompt("Введите число 1 (Задание 4)");
-// let var3_2 = prompt("Введите число 2 (Задание 4)");
+// let prompt4Number1 = prompt("Введите число 1 (Задание 4)");
+// let prompt4Number2 = prompt("Введите число 2 (Задание 4)");
 //
-// console.log(var3_1 < var3_2 ? var3_2 : var3_1);
+// console.log(prompt4Number1 < prompt4Number2 ? prompt4Number2 : prompt4Number1);
 
-// // N-5
+// // N-5`
 //
-// let var5_1 = prompt("Введите число 1 (Задание 5)");
-// let var5_2 = prompt("Введите число 2 (Задание 5)");
+// let prompt5Num1 = prompt("Введите число 1 (Задание 5)");
+// let prompt5Num2 = prompt("Введите число 2 (Задание 5)");
 //
-// console.log(var5_1 % var5_2 == 0)
+// console.log(prompt5Num1 % prompt5Num2 == 0)
 
 // // N-6
 //
-// let var6 = prompt("Введите средний балл (Задание 6)");
+// let prompt6 = prompt("Введите средний балл (Задание 6)");
 //
-// if (var6 >= 1 && var6 <=4) {
+// if (prompt6 >= 1 && prompt6 <=4) {
 //     console.log("Двоечник, иди учись!");
 // }
-// else if (var6 >= 5 && var6 <= 8) {
+// else if (prompt6 >= 5 && prompt6 <= 8) {
 //     console.log("Неплохо, но давай еще поднажмем!");
 // }
-// else if (var6 >= 9 && var6 <= 10) {
+// else if (prompt6 >= 9 && prompt6 <= 10) {
 //     console.log("Ого, да ты настоящий отличник!");
 // }
 
 // // N-7
 //
-// let var7_1 = Number(prompt("Балл за экзамен (от 0 до 100, Задание 7)"));
-// let var7_2 = Number(prompt("Количество выполненных проектов (от 0 и больше, Задание 7)"));
+// let prompt7Num1 = Number(prompt("Балл за экзамен (от 0 до 100, Задание 7)"));
+// let prompt7Num2 = Number(prompt("Количество выполненных проектов (от 0 и больше, Задание 7)"));
 //
-// if (var7_1 >= 90 && var7_2 >= 10) {
+// if (prompt7Num1 >= 90 || prompt7Num2 >= 10) {
 //     var7_total = 100;
-// } else if (var7_1 >= 75 && var7_2 >= 5) {
+// } else if (prompt7Num1 >= 75 && prompt7Num2 >= 5) {
 //     var7_total = 90;
 // }
-// else if(var7_1 >= 50 && var7_2 >= 2) {
+// else if(prompt7Num1 >= 50 && prompt7Num2 >= 2) {
 //     var7_total = 75;
 // }
 // else {
@@ -73,31 +73,31 @@
 
 // // N-8
 //
-// let var8_1 = Number(prompt("Введите количество дней аренды (Задание 8)"));
+// let prompt8 = Number(prompt("Введите количество дней аренды (Задание 8)"));
 //
 // let pricePerDay = 40;
-// let costNormal = pricePerDay * var8_1;
+// let costNormal = pricePerDay * prompt8;
 //
-// var8_1 >= 7 ? costTotal = costNormal - 50 : costTotal = costNormal;
-// var8_1 >= 3 && var8_1 < 7 ? costTotal = costNormal - 20 : costTotal = costNormal;
+// let costTotal = prompt8 >= 7 ? costNormal - 50 : costNormal;
+// costTotal = prompt8 >= 3 && prompt8 < 7 ? costNormal - 20 : costNormal;
 //
 // console.log(costTotal);
 
 // // N-9
 //
-// let var9_1 = Number(prompt("Введите число (Задание 9)"));
+// let prompt9 = Number(prompt("Введите число (Задание 9)"));
 //
 // for(let i = 1 ; i <= 10 ; i++) {
-//     var9_1++;
+//     prompt9++;
 // }
 //
-// console.log(var9_1);
+// console.log(prompt9);
 
 // // N-10
 //
 // for (let i  = 1 ; i <= 10 ; i += 2) {
-//     let var10_1 = Number(prompt("Введите число (Задание 10)"));
-//     var10_1 == 10 ? console.log("Равно 10") : console.log("Не равно 10");
+//     let prompt10 = Number(prompt("Введите число (Задание 10)"));
+//     prompt10 == 10 ? console.log("Равно 10") : console.log("Не равно 10");
 // }
 
 // // N-11
