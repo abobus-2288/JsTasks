56066b3e367be74f8cb5e478b3aebffe68295727
EugeneLobach <ucta456@gmail.com>
2022-11-13 11:31:13 +0300

    7Lesson

---
+++ 7.js
@@ -0,0 +1,15 @@
+// // N-1
+// let numbers = [5.4, 5.5, 5.6, 5.9];
+//
+// for (let number of numbers) {
+//     console.log(`Round ${number}: ${Math.round(number)}`); //Округляет в большую сторону начиная с 0.5 
+//     console.log(`Floor ${number}: ${Math.floor(number)}`); //Округляет в меньшую сторону всегда
+//     console.log(`Ceil ${number}: ${Math.ceil(number)}`); //Округляет в большую сторону всегда
+//     console.log("-----");
+// }
+
+// // N-2
+// let date = new Date();
+//
+// console.log(`Сегодня ${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`); //Методы для получения даты
+// console.log(`${date.getHours()} часов ${date.getMinutes()} минут`);
